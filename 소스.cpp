a85623a5c2ddae510952d7ea08f952f0c62d55d4
이신우 <128461193+Satellite1218@users.noreply.github.com>
2023-07-18 00:01:55 +0900

    학교 방학숙제 codeup 오늘자 최종으로 푼 문제
    뭔 고딩한테 방학숙제를 내고있어..

---
+++ 소스.cpp
@@ -0,0 +1,20 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+int n;
+int zero(int x) {
+	return !x;
+}
+int plus(int x) {
+	if (x > 0) {
+		return x;
+	}
+	else {
+		return 0;
+	}
+}
+int main() {
+	scanf("%d", &n);
+	if (zero(n)) printf("zero");
+	else printf("%s", plus(n) ? "plus" : "minus");
+	return 0;
+}
